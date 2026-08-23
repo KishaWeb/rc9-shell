ce7d103dacb9f7b3134f1edef1ea20ca396fada2
KishaWeb <arshia271@proton.me>
2026-08-23 20:59:55 +0000

    made cd work and changed the promt

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,44 @@
 use std::io::{self, Write};
+use std::process::Command;
+use std::env::{self};
 
 fn main() {
-    loop {
-        print!("rune> ");
+    
+    let home = env::var("HOME").unwrap();
+    env::set_current_dir(home).unwrap();
+    
+    loop {        
+        let dir = env::current_dir().unwrap();
+        print!("{}% ",dir.display());
         io::stdout().flush().unwrap();
 
         let mut input = String::new();
-
-        io::stdin()
-            .read_line(&mut input)
-            .unwrap();
+        io::stdin().read_line(&mut input).unwrap();
 
         let input = input.trim();
+        
+        let mut parts = input.split_whitespace();
+        let command = match parts.next() {
+            Some(cmd) => cmd,
+            None => continue,
+        };
 
+        let args: Vec<&str> = parts.collect();
+        
         if input == "exit" {
             break;
-        }
-
-        if input.is_empty() {
+        }else if command == "cd" {
+            if let Some(path) = args.first() {
+                if let Err(e) = env::set_current_dir(path) {
+                    eprintln!("cd: {e}");
+                }
+            }
             continue;
         }
-
-        println!("You entered: {input}");
+                
+        match Command::new(command).args(args).status() {
+            Ok(status) => println!("exit: {status}"),
+            Err(e) => eprintln!("error: {e}"),
+        }
     }
 }
